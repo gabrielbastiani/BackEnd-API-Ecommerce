e2f8eddee808dda133f719f6c9bd896787a679ef
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-02-22 21:44:06 -0300

    'product'

--- src/services/product/PageListAllProductService.ts
+++ src/services/product/PageListAllProductService.ts
@@ -8,35 +8,15 @@ class PageListAllProductService {
         const allProducts = await prismaClient.product.findMany({
             orderBy: {
                 created_at: 'desc'
+            },
+            include: {
+                category: true,
             }
         });
-
+        
         const products = await prismaClient.product.findMany({
-            select: {
-                id: true,
-                nameProduct: true,
-                descriptionProduct1: true,
-                descriptionProduct2: true,
-                descriptionProduct3: true,
-                descriptionProduct4: true,
-                descriptionProduct5: true,
-                descriptionProduct6: true,
-                preco: true,
-                sku: true,
-                estoque: true,
-                pesoKG: true,
-                larguraCM: true,
-                alturaCM: true,
-                profundidadeCM: true,
-                disponibilidade: true,
-                promocao: true,
-                category_id: true,
-                categoryName: true,
-                loja_id: true,
-                created_at: true,
-                variacoes: true,
-                photoproducts: true,
-                avaliacoes: true
+            include: {
+                category: true,
             },
             skip,
             take: limit,
