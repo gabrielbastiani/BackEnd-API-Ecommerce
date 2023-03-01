aac643e764f3f84f058191a181afc36ab5c90471
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-02-28 22:08:44 -0300

    'fotos'

--- src/services/product/photoproduct/AllPhotosProductService.ts
+++ src/services/product/photoproduct/AllPhotosProductService.ts
@@ -11,6 +11,7 @@ class AllPhotosProductService {
                 product_id: product_id
             },
             select: {
+                id: true,
                 photo: true
             }
         });
