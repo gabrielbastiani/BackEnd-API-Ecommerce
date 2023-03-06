5af4f95e14ac7e63cb48440c03c8fac45737cd87
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-03-06 15:29:37 -0300

    'product'

--- src/controllers/product/DeleteProductController.ts
+++ src/controllers/product/DeleteProductController.ts
@@ -1,20 +1,15 @@
 import { Request, Response } from "express";
 import { DeleteProductService } from "../../services/product/DeleteProductService";
-import fs from 'fs';
 
 class DeleteProductController {
   async handle(req: Request, res: Response) {
     const product_id = req.query.product_id as string;
-    const { variacao_id } = req.body;
 
     const deleteProductService = new DeleteProductService();
 
     const product = await deleteProductService.execute({
       product_id,
-      variacao_id
     });
-
-    /* fs.unlinkSync(__dirname + '/' + '..' + '/' + '..' + '/' + '..' + '/' + 'images' + '/' + ); */
 
     return res.json(product);
   }
--- src/services/product/DeleteProductService.ts
+++ src/services/product/DeleteProductService.ts
@@ -2,33 +2,13 @@ import prismaClient from "../../prisma";
 
 interface ProductRequest {
   product_id: string;
-  variacao_id: string;
-
 }
 
 class DeleteProductService {
-  async execute({ product_id, variacao_id }: ProductRequest) {
+  async execute({ product_id }: ProductRequest) {
     const productDelete = await prismaClient.product.delete({
-      where: {/* @ts-ignore */
+      where: {
         id: product_id
-      }
-    })
-
-    await prismaClient.variacao.delete({
-      where: {/* @ts-ignore */
-        product_id: variacao_id
-      }
-    })
-
-    await prismaClient.photoProduct.delete({
-      where: {/* @ts-ignore */
-        product_id: product_id
-      }
-    })
-
-    await prismaClient.photoVariacao.delete({
-      where: {/* @ts-ignore */
-        product_id: product_id
       }
     })
 
