eccb919b6b6cc726eb9cee5392c5629b3303a2a6
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-03-15 14:32:33 -0300

    'login'

--- src/server.ts
+++ src/server.ts
@@ -6,28 +6,28 @@ import path from 'path';
 
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 app.use(router);
 
 app.use(
     '/files',
     express.static(path.resolve(__dirname, '..', 'images'))
-)
+);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
         //Se for uma instancia do tipo error
         return res.status(400).json({
             error: err.message
-        })
+        });
     }
 
     return res.status(500).json({
         status: 'error',
         message: 'Internal server error.'
-    })
-})
+    });
+});
 
-app.listen(process.env.PORT || 3333, () => console.log('Servidor online!!!!'))
+app.listen(process.env.PORT || 3333, () => console.log('Servidor online!!!!'));
