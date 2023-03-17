5d387164a0e959a175a7b992197470bf49b9f17a
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-03-17 16:00:36 -0300

    'news'

--- src/services/user/UserUpdateNewslatterService.ts
+++ src/services/user/UserUpdateNewslatterService.ts
@@ -1,57 +1,22 @@
-import prismaClient from "../../prisma";
+import prismaClient from '../../prisma';
 
-interface UserRequest{
-  user_id: string;
+interface UserRequest {
+  user_id: any;
+  newslatter: string;
 }
 
 class UserUpdateNewslatterService {
-  async execute({ user_id }: UserRequest) {
-
-    const activeTrue = await prismaClient.user.findUnique({
+  async execute({ user_id, newslatter }: UserRequest) {
+    const userUpdated = await prismaClient.user.update({
       where: {
-        id: user_id
+        id: String(user_id),
       },
-      select: {
-        newslatter: true
+      data: {
+        newslatter: newslatter,
       }
     })
 
-    const activeFalse = await prismaClient.user.findUnique({
-      where: {
-        id: user_id
-      },
-      select: {
-        newslatter: true
-      }
-    })
-
-    if(activeTrue.newslatter === true){
-      const isFalse = await prismaClient.user.update({
-        where:{
-          id: user_id
-        },
-        data: {
-          newslatter: false
-        }
-      })
-
-      return isFalse;
-    }
-
-    if(activeFalse.newslatter === false){
-      const isTrue = await prismaClient.user.update({
-        where:{
-          id: user_id
-        },
-        data: {
-          newslatter: true
-        }
-      })
-
-      return isTrue;
-
-    }
-
+    return userUpdated;
   }
 }
 
