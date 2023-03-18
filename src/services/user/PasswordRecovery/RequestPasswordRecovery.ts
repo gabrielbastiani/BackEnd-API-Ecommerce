eb432835fa24431ad4adec4bacfd9dcbd4ec1126
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-03-17 22:30:51 -0300

    'news'

--- src/services/user/PasswordRecovery/RequestPasswordRecovery.ts
+++ src/services/user/PasswordRecovery/RequestPasswordRecovery.ts
@@ -37,6 +37,31 @@ class RequestPasswordRecovery {
       }
     })
 
+    if(user.role === "USER") {
+      await transporter.sendMail({
+        from: 'Loja virtual Builder Seu Negocio Online <contato@builderseunegocioonline.com.br>',
+        to: user.email,
+        subject: "Recuperação de senha",
+        html: `<div style="background-color: rgb(223, 145, 0); color: black; padding: 0 55px;">
+                  <h2>Recupere sua senha!</h2>
+              </div>
+              
+              <article>
+                  <p>Olá, ${user.nameComplete}!</p>
+                  <p>Voce esqueceu a sua senha?</p>
+                  <p><a href="http://localhost:3001/recover/${recovery.id}">CLIQUE AQUI</a>, para crair uma nova senha de acesso.</p>
+                  <p>Você será redirecionado a uma página em nossa Loja virtual onde poderá cadastrar uma nova senha com segurança!</p>
+              </article>
+              
+              <div style="background-color: rgb(223, 145, 0); color: black; padding: 0 55px;">
+                  <h5>Loja virtual Builder Seu Negocio Online</h5>
+              </div>`,
+      });
+
+      return;
+
+    }
+
     await transporter.sendMail({
       from: 'Loja virtual Builder Seu Negocio Online <contato@builderseunegocioonline.com.br>',
       to: user.email,
