52b297fe611c23995e4370af8490d34e6bfb085f
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-03-23 15:34:46 -0300

    'photos'

--- src/services/product/photoproduct/AllPhotosProductInStoreService.ts
+++ src/services/product/photoproduct/AllPhotosProductInStoreService.ts
@@ -3,9 +3,8 @@ import prismaClient from '../../../prisma';
 class AllPhotosProductInStoreService {
     async execute() {
         const allProducts = await prismaClient.photoProduct.findMany({
-            select: {
-                id: true,
-                photo: true
+            include: {
+                product: true,
             }
         });
 
