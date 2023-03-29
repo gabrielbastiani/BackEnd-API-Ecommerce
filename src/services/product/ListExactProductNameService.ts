e3db943d21748f56ce2ca0616a7d7bbadf1d14ee
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-03-28 22:08:15 -0300

    'exact'

--- src/services/product/ListExactProductNameService.ts
+++ src/services/product/ListExactProductNameService.ts
@@ -1,11 +1,16 @@
 import prismaClient from '../../prisma';
 
-
 class ListExactProductNameService {
     async execute({ slug }) {
         const exactProduct = await prismaClient.product.findUnique({
             where: {
                 slug
+            },
+            include: {
+                photoproducts: true,
+                variacoes: true,
+                photovariacoes: true,
+                avaliacoes: true,
             }
         })
         return exactProduct;
