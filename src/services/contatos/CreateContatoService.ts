d2312ba271b4bcd2632c8629a17039b7dcfdec08
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-03-31 10:22:11 -0300

    news

--- src/services/contatos/CreateContatoService.ts
+++ src/services/contatos/CreateContatoService.ts
@@ -1,4 +1,7 @@
 import prismaClient from "../../prisma";
+import nodemailer from "nodemailer";
+require('dotenv/config');
+
 
 interface ContatoRequest {
   name: string;
@@ -23,6 +26,30 @@ class CreateContatoService {
         loja_id: loja_id
       }
     })
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.HOST_SMTP,
+      port: 465,
+      auth: {
+        user: process.env.USER_SMTP,
+        pass: process.env.PASS_SMTP
+      }
+    })
+
+    await transporter.sendMail({
+      from: "Loja Virtual - Builder Seu Negocio Online <contato@builderseunegocioonline.com.br>",
+      to: `${email}`,
+      subject: "Recebemos seu contato",
+      html: `
+            <article>
+                <p>Olá ${name}!</p>
+                <p>Agradecenos por entrar em contato conosco, fique tranquilo(a), em breve retornaremos contato por aqui, ou pelo telefone que nos deixou.</p>
+            </article>
+            
+            <div style="background-color: rgb(223, 145, 0); color: black; padding: 0 55px;">
+                <h5>Loja Virtual Builder Seu Negocio Online</h5>
+            </div>`,
+    });
 
     return contato;
 
--- src/services/newsletters/CreateNewslettersService.ts
+++ src/services/newsletters/CreateNewslettersService.ts
@@ -1,4 +1,7 @@
 import prismaClient from "../../prisma";
+import nodemailer from "nodemailer";
+require('dotenv/config');
+
 
 interface NewsRequest {
   name: string;
@@ -15,6 +18,30 @@ class CreateNewslettersService {
         loja_id: loja_id
       }
     })
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.HOST_SMTP,
+      port: 465,
+      auth: {
+        user: process.env.USER_SMTP,
+        pass: process.env.PASS_SMTP
+      }
+    })
+
+    await transporter.sendMail({
+      from: "Loja Virtual - Builder Seu Negocio Online <contato@builderseunegocioonline.com.br>",
+      to: `${email}`,
+      subject: "Em breve novidades da nossa loja virtual para você",
+      html: `
+            <article>
+                <p>Olá ${name}!</p>
+                <p>Agradecemos pelo seu cadastro em nossa newsletters da loja virtual Builder Seu Negócio Online, em breve novidades por aqui aguarde!!!.</p>
+            </article>
+            
+            <div style="background-color: rgb(223, 145, 0); color: black; padding: 0 55px;">
+                <h5>Loja Virtual Builder Seu Negocio Online</h5>
+            </div>`,
+    });
 
     return newsletter;
 
