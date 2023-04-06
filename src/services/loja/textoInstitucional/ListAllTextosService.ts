2df34a0081b4e774e6c4ce7272391890e4f28f62
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-04-06 19:07:08 -0300

    categorys

--- src/services/loja/textoInstitucional/ListAllTextosService.ts
+++ src/services/loja/textoInstitucional/ListAllTextosService.ts
@@ -3,6 +3,9 @@ import prismaClient from "../../../prisma";
 class ListAllTextosService {
    async execute() {
       const allTextos = await prismaClient.textoInstitucional.findMany({
+         include: {
+            imagesloja: true
+         },
          orderBy: {
             order: 'asc'
          }
