a8add3d25fdb6bfaad9c84b2529a361293ad08b1
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-04-18 17:23:48 -0300

    contato

--- src/services/relationProductCategory/DeleteRelationIDProductCategoryService.ts
+++ src/services/relationProductCategory/DeleteRelationIDProductCategoryService.ts
@@ -6,6 +6,26 @@ interface RelationRequest {
 
 class DeleteRelationIDProductCategoryService {
     async execute({ relationProductCategory_id }: RelationRequest) {
+
+        const idNivel = await prismaClient.relationProductCategory.findUnique({
+            where: {
+                id: relationProductCategory_id
+            }
+        });
+
+        const idNivelRelations = await prismaClient.relationProductCategory.findMany();
+        const idsRelat = idNivelRelations.map((item) => item.relationId);
+
+        const filtrado = idsRelat.filter(function (objet) { return idNivel.id === objet });
+
+        console.log(idNivel.id)
+
+        console.log(String(filtrado))
+
+        if (String(filtrado) != idNivel.id) {
+            throw new Error("Delete as categorias filhas desta de trás para frente antes!!!");
+        }
+
         const relation = await prismaClient.relationProductCategory.delete({
             where: {
                 id: relationProductCategory_id
