97bf62ccc6d77192278d36a895fd8f4a69e2e310
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-05-07 17:59:36 -0300

    atributos

--- src/controllers/filtro/FiltroAtributosAndCategoriesController.ts
+++ src/controllers/filtro/FiltroAtributosAndCategoriesController.ts
@@ -3,12 +3,15 @@ import { FiltroAtributosAndCategoriesServices } from "../../services/filtro/Filt
 
 class FiltroAtributosAndCategoriesController {
   async handle(req: Request, res: Response) {
+    const slug = req.query.slug as any;
+    const slugValor = req.query.slugValor as any;
 
     const filters = new FiltroAtributosAndCategoriesServices();
 
-    const filter = await filters.execute();
+    const filter = await filters.execute( slug );
+    const filter1 = await filters.execute( slugValor );
 
-    return res.json(filter);
+    return res.json([filter, filter1]);
   }
 }
 
--- src/services/filtro/FiltroAtributosAndCategoriesServices.ts
+++ src/services/filtro/FiltroAtributosAndCategoriesServices.ts
@@ -1,12 +1,30 @@
 import prismaClient from "../../prisma";
 
+interface FilterRequest {
+    slug: any;
+    slugValor: any;
+}
+
 class FiltroAtributosAndCategoriesServices {
-    async execute() {
+    async execute({ slug, slugValor }: FilterRequest) {
 
-        const filterCategory = await prismaClient.relationProductCategory.findMany();
-        const filterAtributo = await prismaClient.relationProductAtributo.findMany();
+        const filterCategory = await prismaClient.relationProductCategory.findMany({
+            where: {
+                category: {
+                    slug: slug
+                }
+            }
+        });
 
-        return ([filterCategory, filterAtributo]);
+        await prismaClient.relationProductAtributo.findMany({
+            where: {
+                atributo: {
+                    slugValor: slugValor
+                }
+            }
+        })
+
+        return (filterCategory);
 
     }
 
