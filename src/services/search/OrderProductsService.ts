29077748013905c20283a1fe3634c822d30085b4
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-07-02 17:11:26 -0300

    pagecategory

--- src/services/search/OrderProductsService.ts
+++ src/services/search/OrderProductsService.ts
@@ -1,18 +1,54 @@
 import { StatusProduct } from '@prisma/client';
 import prismaClient from '../../prisma';
 
-interface OrderRequest {
-    sortBy: any;
-    product_id: any;
-}
-
 class OrderProductsService {
-    async execute({ product_id, sortBy }: OrderRequest) {
+    async execute(page = 1, limit = 20, product_id: any, sortBy: any) {
+
+        const skip = limit * (page - 1);
+
+        const allOrdersProducts = await prismaClient.product.findMany({
+            where: {
+                id: { in: product_id },
+                status: StatusProduct.Disponivel,
+            },
+            include: {
+                photoproducts: true
+            }
+        });
+
+        switch (sortBy) {
+            case "produtosDeAaZ":
+                allOrdersProducts.sort((a, z) => a.name.localeCompare(z.name));
+                break;
+            case "produtosDeZaA":
+                allOrdersProducts.sort((a, z) => z.name.localeCompare(a.name));
+                break;
+            case "produtosPrecosMaisAltos":
+                allOrdersProducts.sort((n1, n9) => n9.price - n1.price);
+                break;
+            case "produtosPrecosMaisBaixos":
+                allOrdersProducts.sort((n1, n9) => n1.price - n9.price);
+                break;
+            case "menorPrecoPromocao":
+                allOrdersProducts.sort((o1, o9) => o1.promotion - o9.promotion);
+                break;
+            case "maisAntigos":
+                allOrdersProducts.sort((d1, d2) => new Date(d1.created_at).getTime() - new Date(d2.created_at).getTime());
+                break;
+            case "lancamentos":
+                allOrdersProducts.sort((d1, d2) => new Date(d2.created_at).getTime() - new Date(d1.created_at).getTime());
+                break;
+            default:
+                allOrdersProducts.sort((a, z) => a.name.localeCompare(z.name));
+        }
+
         const ordersProducts = await prismaClient.product.findMany({
             where: {
                 id: { in: product_id },
                 status: StatusProduct.Disponivel,
             },
+            skip,
+            take: limit,
             include: {
                 photoproducts: true
             }
@@ -44,7 +80,14 @@ class OrderProductsService {
                 ordersProducts.sort((a, z) => a.name.localeCompare(z.name));
         }
 
-        return ordersProducts;
+        const data = {
+            ordersProducts,
+            total: allOrdersProducts.length,
+            total_pages: Math.ceil(allOrdersProducts.length / limit),
+            current_page: page,
+        }
+
+        return data;
     }
 }
 
