faed23a7807fe2f4b0e82701720196d3512c878d
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-07-21 15:46:32 -0300

    pageproduct

--- src/controllers/search/FavoritesProductsController.ts
+++ src/controllers/search/FavoritesProductsController.ts
@@ -3,11 +3,11 @@ import { FavoritesProductsService } from '../../services/search/FavoritesProduct
 
 class FavoritesProductsController {
     async handle(req: Request, res: Response) {
-        const { product_id } = req.query;
+        const { page, limit, product_id } = req.query;
 
         const listProducts = new FavoritesProductsService();
 
-        const filterProduct = await listProducts.execute({ product_id });
+        const filterProduct = await listProducts.execute( Number(page), Number(limit), product_id );
 
         return res.json(filterProduct);
     }
--- src/services/search/FavoritesProductsService.ts
+++ src/services/search/FavoritesProductsService.ts
@@ -1,16 +1,18 @@
 import { StatusProduct } from '@prisma/client';
 import prismaClient from '../../prisma';
 
-interface FavoriteRequest {
-    product_id: string;
-}
-
 class FavoritesProductsService {
-    async execute({ product_id }: FavoriteRequest) {
-        const productsFavorites = await prismaClient.product.findMany({
+    async execute(page = 1, limit = 20, product_id) {
+
+        const skip = limit * (page - 1);
+
+        const allProductsFavorites = await prismaClient.product.findMany({
             where: {
                 status: StatusProduct.Disponivel,
                 id: { in: product_id }
+            },
+            orderBy: {
+                created_at: 'desc'
             },
             include: {
                 avalietions: true,
@@ -26,7 +28,38 @@ class FavoritesProductsService {
             }
         });
 
-        return productsFavorites;
+        const productsFavorites = await prismaClient.product.findMany({
+            where: {
+                status: StatusProduct.Disponivel,
+                id: { in: product_id }
+            },
+            skip,
+            take: limit,
+            orderBy: {
+                created_at: 'desc'
+            },
+            include: {
+                avalietions: true,
+                buyTogether: true,
+                buytogethers: true,
+                descriptionproducts: true,
+                photoproducts: true,
+                productcategories: true,
+                relationattributeproducts: true,
+                store: true,
+                tags: true,
+                variations: true
+            }
+        });
+
+        const data = {
+            productsFavorites,
+            total: allProductsFavorites.length,
+            total_pages: Math.ceil(allProductsFavorites.length / limit),
+            current_page: page,
+        }
+
+        return data;
         
     }
 }
