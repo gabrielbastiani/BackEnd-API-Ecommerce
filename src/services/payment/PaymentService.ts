59fcf1538b10b631627af518b9e1de74c753b966
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-08-21 17:08:38 -0300

    cart

--- src/services/payment/PaymentService.ts
+++ src/services/payment/PaymentService.ts
@@ -20,12 +20,33 @@ class PaymentService {
                     picture_url: "http://localhost:3001/_next/image?url=http%3A%2F%2Flocalhost%3A3333%2Ffiles%2Fa412a3aad8af04a4018463cdfaf74754-hawk255-2.png&w=640&q=75"
                 }
             ],
+            payer: {
+                name: "<PAYER_NAME_HERE>",
+                surname: "<PAYER_SURNAME_HERE>",
+                email: "<PAYER_EMAIL_HERE>",
+                phone: {
+                    area_code: "<PAYER_AREA_CODE_HERE>",
+                    number: "<PAYER_PHONE_NUMBER_HERE>"
+                },
+                identification: {
+                    type: "<PAYER_DOC_TYPE_HERE>",
+                    number: "<PAYER_DOC_NUMBER_HERE>"
+                },
+                address: {
+                    street_name: "Street",
+                    street_number: 123,
+                    zip_code: "<PAYER_ZIP_CODE_HERE>"
+                }
+            },
+            payment_methods: {
+                installments: 12
+            },
             back_urls: {
                 success: "http://localhost:3333/success",
                 failure: "http://localhost:3333/failure",
                 pending: "http://localhost:3333/pending"
             },
-            notification_url: "https://d7ae-177-69-27-241.ngrok.io/webhook"
+            notification_url: "https://d7ae-177-69-27-241.ngrok.io/webhook",
         });
 
         return result;
