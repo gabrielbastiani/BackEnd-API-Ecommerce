ded273d772926edc5219fd344d96d016a41fad69
Gabriel Campos de Bastiani <gabriel.bastiani@sumig.local>
2023-08-24 17:19:35 -0300

    cart

--- src/controllers/payment/PaymentController.ts
+++ src/controllers/payment/PaymentController.ts
@@ -1,36 +1,25 @@
 import { Request, Response } from "express";
-import { PaymentService } from "../../services/payment/PaymentService";
+/* import { PaymentService } from "../../services/payment/PaymentService"; */
+import mercadopago from 'mercadopago';
+mercadopago.configure({
+    access_token: process.env.ACCESS_TOKEN_TEST
+});
 
 class PaymentController {
     async handle(req: Request, res: Response) {
 
-        const {
-            description,
-            email,
-            type_identification,
-            type_number,
-            installments,
-            payment_method_id,
-            transaction_amount,
-            issuer_id,
-            unit_price,
-        } = req.body;
+        console.log(req.body)
 
-        const payments = new PaymentService();
-
-        const paymentsResults = await payments.execute({
-            description,
-            email,
-            type_identification,
-            type_number,
-            installments,
-            payment_method_id,
-            transaction_amount,
-            issuer_id,
-            unit_price,
+        mercadopago.payment.save(req.body)
+        .then(function(response) {
+            const { status, status_detail, id } = response.body;
+            res.status(response.status).json({ status, status_detail, id });
+            console.log(status, status_detail, id)
+        })
+        .catch(function(error) {
+            console.error(error);
         });
 
-        return res.json(paymentsResults);
     }
 }
 
--- src/services/payment/PaymentService.ts
+++ src/services/payment/PaymentService.ts
@@ -1,85 +1,51 @@
-import mercadopago from 'mercadopago';
+/* import mercadopago from 'mercadopago';
+import prismaClient from '../../prisma';
 mercadopago.configure({
     access_token: process.env.ACCESS_TOKEN_TEST
 });
 
 interface PaymentRequest {
-    description: string;
-    email: string;
-    type_identification: string;
-    type_number: string;
-    installments: number;
+    token: string;
+    issuer_id: string;
     payment_method_id: string;
     transaction_amount: number;
-    issuer_id: string;
-    unit_price: number;
+    installments: number;
+    email: string;
+    identificationType: string;
+    identificationNumber: string;
 }
 
 class PaymentService {
     async execute({
-        description,
-        email,
-        type_identification,
-        type_number,
-        installments,
+        token,
+        issuer_id,
         payment_method_id,
         transaction_amount,
-        issuer_id,
-        unit_price,
+        installments,
+        email,
+        identificationType,
+        identificationNumber
     }: PaymentRequest) {
 
-        const resultCreate = await mercadopago.preferences.create({
-            items: [
-                {
-                    title: "Teste",
-                    unit_price: 1000,
-                    currency_id: "BRL",
-                    quantity: 1,
-                    category_id: "Solda",
-                    description: "LojaSolda",
-                }
-            ],
-            payer: {
+        const buy = await prismaClient.product.create({
+            data: {
+                token: token,
+                issuer_id: issuer_id,
+                payment_method_id: payment_method_id,
+                transaction_amount: Number(transaction_amount),
+                installments: Number(installments),
+                description: "Descrição do produto",
                 email: email,
-                identification: {
-                    type: type_identification,
-                    number: type_number
-                }
-            },
-            payment_methods: {
-                installments: 12
-            },
-            back_urls: {
-                success: "http://localhost:3001/success",
-                failure: "http://localhost:3001/failure",
-                pending: "http://localhost:3001/pending"
-            },
-            notification_url: "https://d7ae-177-69-27-241.ngrok.io/webhook",
-        });
-
-        const create = resultCreate.body;
-
-        const result = await mercadopago.payment.save({
-            installments: 1,
-            payment_method_id: payment_method_id,
-            transaction_amount: transaction_amount,
-            description: description,
-            issuer_id: issuer_id,
-            token: issuer_id,
-            payer: {
-                email: email,
-                identification: {
-                    type: type_identification,
-                    number: type_number
-                }
+                type: identificationType,
+                number: identificationNumber,
             }
         });
 
-        console.log(create)
+        console.log(buy)
 
-        return result;
+        return buy;
 
     }
 }
 
-export { PaymentService }
+export { PaymentService } */
