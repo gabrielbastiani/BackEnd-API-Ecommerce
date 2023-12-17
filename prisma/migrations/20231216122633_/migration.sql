fab875e5c52eb2d75f9fb355687e9e103d9e3744
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2023-12-16 22:45:28 -0300

    store

---
+++ prisma/migrations/20231216122633_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `total_payment_juros` on the `payments` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "payments" DROP COLUMN "total_payment_juros";
